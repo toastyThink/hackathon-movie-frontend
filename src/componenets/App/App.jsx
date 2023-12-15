c1b74235fed31153ab0c8ade273ba23a99a5e194
aobarbieri <aobarbieri@gmail.com>
2023-12-15 14:54:26 -0500

    Initial styles

--- src/componenets/App/App.jsx
+++ src/componenets/App/App.jsx
@@ -1,22 +1,16 @@
-// import { useState } from 'react'
 import Header from '../Header/Header.jsx'
 import Footer from '../Footer/Footer.jsx'
 import MovieList from '../MovieList/MovieList.jsx'
 import './App.css'
 
-// import {Route, Routes} from 'react-router-dom'
-
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <>
-
-    <Header />
-    <MovieList />
-    <Footer />
-    </>
-  )
+	return (
+		<div className='app'>
+			<Header />
+			<MovieList />
+			<Footer />
+		</div>
+	)
 }
 
 export default App
